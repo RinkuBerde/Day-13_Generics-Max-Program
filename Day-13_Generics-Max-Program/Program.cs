1c92a71c78191d6755655d4100cef9673d62d056
rinkuberde <rinkuberde@gmail.com>
2023-01-17 15:06:23 +0530

    [Rinku] -  Welcome Message

---
+++ Day-13_Generics-Max-Program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_13_Generics_Max_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Problem using Generics!!!");
+        }
+    }
+}
